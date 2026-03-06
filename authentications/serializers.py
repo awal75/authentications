cae52698afb2b523578862ae5ccd10a883645578
Awal Islam <awal.aaraf92@gmail.com>
2026-03-06 23:21:50 +0600

    add usercreate serializer

--- authentications/serializers.py
+++ authentications/serializers.py
@@ -1,3 +1,35 @@
 from rest_framework import serializers
 import models
+from django.conf import settings
+from django.db import transaction
+from django.contrib.auth import get_user_model
 
+User=User = get_user_model()
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(write_only=True)
+    last_name = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            # 1. Extract profile data
+            first_name = validated_data.pop('first_name')
+            last_name = validated_data.pop('last_name')
+
+            # 2. Create the User first (using the manager to hash password)
+            user = User.objects.create_user(**validated_data)
+
+            # 3. Create the Profile linked to the new User
+            models.Profile.objects.create(
+                user=user, 
+                first_name=first_name, 
+                last_name=last_name
+            )
+
+            return user
