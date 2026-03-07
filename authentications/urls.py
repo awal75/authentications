0be24335a29d7b731c9da8b66e096ee9ff9572bd
Awal Islam <awal.aaraf92@gmail.com>
2026-03-07 23:12:27 +0600

    routers

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserModelViewSet
+from .views import UserModelViewSet,ProfileModelViewSet
 
 router=DefaultRouter()
 router.register('users',UserModelViewSet,basename='users')
+router.register('profiles',ProfileModelViewSet,basename='profiles')
 
 
 urlpatterns = router.urls
