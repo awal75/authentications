f0c3d48c343d1dbf8234489a2b3ab7950a69314f
Awal Islam <awal.aaraf92@gmail.com>
2026-03-07 23:10:57 +0600

    add profile view

--- authentications/urls.py
+++ authentications/urls.py
@@ -4,4 +4,5 @@ from .views import UserModelViewSet
 router=DefaultRouter()
 router.register('users',UserModelViewSet,basename='users')
 
+
 urlpatterns = router.urls
--- authentications/views.py
+++ authentications/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from .serializers import UserCreateSerializer,ProfileSerilizer
 from django.contrib.auth import get_user_model
-from .serializers import UserCreateSerializer
-
-
+from .models import Profile
 User=get_user_model()
 
 class UserModelViewSet(ModelViewSet):
     queryset=User.objects.all()
     serializer_class=UserCreateSerializer
+
+class ProfileModelViewSet(ModelViewSet):
+    queryset=Profile.objects.all()
+    serializer_class=ProfileSerilizer
