37b2f185888238285e74119d6eac97c0a3d70374
Awal Islam <awal.aaraf92@gmail.com>
2026-04-10 22:51:00 +0600

    stack con

--- authentications/views.py
+++ authentications/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import UserCreateSerializer,ProfileSerilizer
+from .serializers import UserCreateSerializer,ProfileSerilizer,ProfileUpdateSerilizer
 from django.contrib.auth import get_user_model
 from .models import Profile
+from rest_framework import permissions
 User=get_user_model()
 
 class UserModelViewSet(ModelViewSet):
     queryset=User.objects.all()
     serializer_class=UserCreateSerializer
+    def get_permissions(self):
+        if self.action== 'create':
+            return [permissions.AllowAny]
+        elif self.action=='list':
+            return [permissions.IsAdminUser]
+        return [permissions.IsAdminUser]
 
 class ProfileModelViewSet(ModelViewSet):
     queryset=Profile.objects.all()
-    serializer_class=ProfileSerilizer
+
+    def get_serializer_class(self):
+        if self.action=='update':
+            return ProfileUpdateSerilizer
+        return ProfileSerilizer
