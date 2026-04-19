7d4e3016b4c664a6230e47da182bf491ec23876f
Awal Islam <awal.aaraf92@gmail.com>
2026-04-20 01:05:45 +0800

    working

--- authentications/views.py
+++ authentications/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet,GenericViewSet
 from .serializers import UserCreateSerializer,ProfileSerilizer,ProfileUpdateSerilizer
 from django.contrib.auth import get_user_model
 from .models import Profile
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
+from rest_framework import mixins
 from .serializers import JwtSerialzer
 User=get_user_model()
 
@@ -18,9 +19,9 @@ class UserModelViewSet(ModelViewSet):
             return [permissions.IsAdminUser]
         return [permissions.IsAuthenticated]
 
-class ProfileModelViewSet(ModelViewSet):
+class ProfileModelViewSet(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,GenericViewSet):
     queryset=Profile.objects.all()
-
+    
     def get_serializer_class(self):
         if self.action=='update':
             return ProfileUpdateSerilizer
